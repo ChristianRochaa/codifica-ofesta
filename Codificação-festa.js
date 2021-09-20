6883feee325f845674c5a84c0e3cf6547ac27cc5
ChristianRochaa <89591986+ChristianRochaa@users.noreply.github.com>
2021-09-20 00:07:26 -0300

    Add files via upload

---
+++ Codificação-festa.js
@@ -0,0 +1,19 @@
+var  dataEvento = Date ;
+console.log(dataEvento);
+    if (dataEvento <= Date )
+console.log('cadastro permitido por data valida');
+    else (dataEvento > Date)
+console.log ('Não permitido data excedida');
+
+var idadePermitida = 18;
+   if (idadePermitida >= 18)
+console.log('Permitido por idade');
+   else idadePermitida < 18
+console.log('Não permitida');
+
+var quantidadeDeParticipantes = 100;
+console.log(quantidadeDeParticipantes);
+if (quantidadeDeParticipantes <= 101)
+console.log('Cadastro permitido');
+else (quantidadeDeParticipantes > 100)
+console.log('cadastro não permitido por ter excedido limite de pessoas');
